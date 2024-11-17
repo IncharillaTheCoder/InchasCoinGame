ba2bc05d316e97063c30138406a7df9b8fdfb6e0
IncharillaTheCoder <elias.win@hotmail.com>
2024-11-17 20:41:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ document.addEventListener("keydown", function(event) {
     var coin = document.getElementById("coin");
     var result = document.getElementById("result");
 
-    if (event.key === "h" || event.key === "H") {
-        coin.style.transform = "rotateY(0deg)";
-        result.innerHTML = "Heads";
-    } else if (event.key === "t" || event.key === "T") {
-        coin.style.transform = "rotateY(180deg)";
-        result.innerHTML = "Tails";
-    }
+    coin.style.transition = "transform 3s";
+    coin.style.transform = "rotateY(360deg)";
+
+    setTimeout(function() {
+        if (event.key === "h" || event.key === "H") {
+            coin.style.transform = "rotateY(0deg)";
+            result.innerHTML = "Heads";
+        } else if (event.key === "t" || event.key === "T") {
+            coin.style.transform = "rotateY(180deg)";
+            result.innerHTML = "Tails";
+        }
+        coin.style.transition = "";
+    }, 3000);
 });
