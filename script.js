317fc25b9dcd778e229e7aeef574115fab35460f
IncharillaTheCoder <elias.win@hotmail.com>
2024-11-17 21:00:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ document.addEventListener("keydown", function(event) {
     var coin = document.getElementById("coin");
     var result = document.getElementById("result");
 
-    coin.style.transition = "transform 4s";
-    coin.style.transform = "rotateY(1440deg)"; // Rotate for 4 seconds
+    coin.style.transition = "transform 4s linear";
+    coin.style.transform = "rotateY(1440deg)"; // 1440deg to rotate continuously for 4 seconds
+
+    result.innerHTML = ""; // Clear result until coin stops spinning
 
     setTimeout(function() {
         if (event.key === "h" || event.key === "H") {
+            coin.style.transition = "none"; // Stop spinning
             coin.style.transform = "rotateY(0deg)";
             result.innerHTML = "Heads";
         } else if (event.key === "t" || event.key === "T") {
+            coin.style.transition = "none"; // Stop spinning
             coin.style.transform = "rotateY(180deg)";
             result.innerHTML = "Tails";
         }
-        coin.style.transition = "";
     }, 4000);
-
-    result.innerHTML = ""; // Clear result until coin stops spinning
 });
